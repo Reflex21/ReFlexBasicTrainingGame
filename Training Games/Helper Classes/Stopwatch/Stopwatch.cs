07442ba7c61f8f3bf6d3c2ae4c8f76ff9d23b6bd
Anthony I. Stewart <anthonystewart1129@gmail.com>
2021-02-15 21:58:16 -0500

    Reorganized files

---
+++ Training Games/Helper Classes/Stopwatch/Stopwatch.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Utilities
+{
+	public class StopwatchWrapper
+	{
+		public double total_time;
+		public double calls;
+		private Stopwatch sw;
+
+		public StopwatchWrapper()
+		{
+			sw = new Stopwatch();
+		}
+
+		public void reset()
+		{
+			total_time = 0;
+			calls = 0;
+			sw.Reset();
+		}
+
+		public void start()
+		{
+			sw.Start();
+			calls++;
+		}
+
+		public void stop()
+		{
+			sw.Stop();
+			total_time += (double)sw.Elapsed.TotalMilliseconds;
+			sw.Reset();
+		}
+
+		public double latency()
+		{
+			return total_time;
+		}
+
+		public double avg_latency()
+		{
+			return (total_time / calls);
+		}
+	}
+}
+
